3e6d2f3e390a20367e361a1adfe45da3f24c02fb
unknown <antmoore311@gmail.com>
2022-02-18 12:38:45 -0500

    New Files

---
+++ Battle.py
@@ -0,0 +1,32 @@
+class Battle:
+    def __init__(self,myPets,otherPets):
+        self.myParty = Party(self, myPets)
+        self.otherParty = Party(self, otherPets)
+    def printBattle(self):
+        lenMyParty = len(self.myParty.partyPets)
+        for i in range(lenMyParty,5):
+            print(" ", end="")
+        self.myParty.printB()
+        print("    ", end="")
+        self.otherParty.printF()
+        print()
+    def startBattle(self):
+        myDamage = self.myParty.startBattle(self.otherParty.len())
+        otherDamage = self.otherParty.startBattle(self.myParty.len())
+        print("Start Battle:", otherDamage, "   ", myDamage)
+        self.myParty.takeDamage(otherDamage)
+        self.otherParty.takeDamage(myDamage)
+    def singleAttack(self):
+        myDamage = self.myParty.getTurnDamage(self.otherParty.len())
+        otherDamage = self.otherParty.getTurnDamage(self.myParty.len())
+        print("Attack: ", otherDamage, "   ", myDamage)
+        self.myParty.takeDamage(otherDamage)
+        self.otherParty.takeDamage(myDamage)
+    def checkOutcome(self):
+        if(self.myParty.isAlive() and self.otherParty.isAlive()):
+            return 69
+        elif(self.myParty.isAlive()):
+            return 1
+        elif(self.otherParty.isAlive()):
+            return -1
+        return 0
